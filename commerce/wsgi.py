b39ed0e1a94df49ce5777501ceddab8d2cd260df
mohsinwarind <mohsinramzan999@gmail.com>
2025-07-21 12:32:13 +0500

    Fixing some things

--- commerce/wsgi.py
+++ commerce/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
 
 application = get_wsgi_application()
+
+app = application
+# This is a WSGI configuration file for a Django project named "commerce".
